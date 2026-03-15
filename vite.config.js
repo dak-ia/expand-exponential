56665660981ff684d19a8973c00715136174ca38
dak-ia <97687749+dak-ia@users.noreply.github.com>
2026-03-16 03:58:55 +0900

    feat: project setup (#1)
    - package.json (expand-exponential)
    - TypeScript, ESLint, Prettier, Jest, Vite configuration
    - Husky + lint-staged
    - MIT License

---
+++ vite.config.js
@@ -0,0 +1,42 @@
+import { defineConfig } from "vite";
+import pkg from "./package.json";
+
+export default defineConfig(({ command }) => {
+  const commonConfig = {
+    define: {
+      VERSION: JSON.stringify(pkg.version),
+    },
+  };
+
+  if (command === "serve") {
+    // dev server
+    return {
+      ...commonConfig,
+      root: "examples",
+      server: {
+        watch: {
+          usePolling: true, // wsl
+        },
+      },
+    };
+  } else {
+    // build
+    return {
+      ...commonConfig,
+      build: {
+        lib: {
+          entry: "./src/index.ts",
+          name: "ExpandExponential",
+          formats: ["es", "cjs", "umd"],
+          fileName: (format) => {
+            if (format === "es") return "index.mjs";
+            if (format === "cjs") return "index.cjs";
+            if (format === "umd") return "index.umd.js";
+          },
+        },
+        outDir: "dist",
+        emptyOutDir: true,
+      },
+    };
+  }
+});
